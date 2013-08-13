e28f822ea04caddb252ad597e25f3a25b8172f1c
Julian Gruber <julian@juliangruber.com>
2013-08-12 23:54:31 -0400

    attribute tests fails when creating attributes

--- test/attributes.js
+++ test/attributes.js
@@ -2,13 +2,14 @@ var test = require('tap').test;
 var hyperstream = require('..');
 var concat = require('concat-stream');
 
-var src = '<div><input value=""></div>';
-var expected = '<div><input value="value"></div>';
+var src = '<div><input value=""><span></span></div>';
+var expected = '<div><input value="value"><span class="class"></span></div>';
 
 test('attributes', function (t) {
     t.plan(1);
     var hs = hyperstream({
-       'input': { value : 'value' } 
+       input : { value : 'value' },
+       span : { 'class': 'class' }
     });
     hs.pipe(concat(function (html) {
         t.equal(html.toString(), expected);
